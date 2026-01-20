4c614fd4a1c19bea9126561c6a06dc7a024c4b66
SaffronG <saffrongamer167@gmail.com>
2026-01-19 20:46:33 -0700

    added namespaces and added a main driver file for cleanliness

--- gamelib.cljs
+++ gamelib.cljs
@@ -1,4 +1,4 @@
-(ns lib)
+(ns gamelib)
 
 ;; custom structs
 (defn init-cell [x y state]
@@ -15,17 +15,28 @@
       :else nil)))
 
 ;; function accessors
-(defn alive? [cell] 
+
+;; cell alive? -> T or F
+(defn alive? [cell]
   (cell 'alive?))
+
+;; cell chek-neighbor -> [x-y chords ...]
 (defn check-neighbor [cell]
   (cell 'check-neighbor))
+
+;; cell evolve -> NOT IMPLEMENTED
 (defn evolve [cell]
   (cell 'evolve))
+
+;; cell get-xy -> [x y]
 (defn get-xy [cell]
   (cell 'get-xy))
+
+;; cell display -> "x y (T or F)"
 (defn display [cell]
   (cell 'display))
 
+;; no longer used, use the next-gen? function for faster lookup speeds
 (defn xy=? [cell this-x this-y]
   (let [x (0 (get-xy cell)) y (1 (get-xy cell))]
     (cond 
@@ -34,11 +45,8 @@
 
 ;; functions to operate on structs
 (defn next-gen? [nb cells]
-  (reduce + (for [[x y] nb] ;; Haskell-like destructuring
-    (if (alive? (get-in cells [y x]))) ;; -> cells[y][x].is_alive();
-      1 ;; T
-      0))) ;; F
-
-(def cs (for [x (range 3) y (range 3)] (init-cell x y false)))
-(def dcs (map display cs))
-(println dcs)
+  (reduce + 
+    (for [[x y] nb] ;; Haskell-like destructuring
+      (if (alive? (get-in cells [y x])) ;; -> cells[y][x].is_alive();
+        1 ;; T
+        0)))) ;; F
--- main.cljs
+++ main.cljs
@@ -1,1 +1,6 @@
-(require 'lib)
+(ns main
+  (:require [gamelib :as gamelib]))
+
+(def cs (for [x (range 3) y (range 3)] (gamelib/init-cell x y false)))
+(def dcs (map gamelib/display cs))
+(println dcs)
