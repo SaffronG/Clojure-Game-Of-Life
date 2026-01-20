6ce96e29323c536956404d55b7be1466bb598f9f
Johan Cajas <johancajas12@gmail.com>
2026-01-19 21:59:16 -0700

    1/19/2026

--- gamelib.cljs
+++ gamelib.cljs
@@ -1,52 +1,55 @@
 (ns gamelib)
 
-;; custom structs
+;; cell as closure (original)
 (defn init-cell [x y state]
   (fn [predicate]
     (cond
-      (= predicate 'alive?) state ;; returns T or F (etc. state)
-      (= predicate 'check-neighbor) 
-        (for [x-off [-1 0 1]
-              y-off [-1 0 1]]
-          [(+ x x-off) (+ y y-off)])
+      (= predicate 'alive?) state
+      (= predicate 'check-neighbor)
+      (for [x-off [-1 0 1]
+            y-off [-1 0 1]]
+        [(+ x x-off) (+ y y-off)])
       (= predicate 'evolve) ()
       (= predicate 'get-xy) [x y]
-      (= predicate 'display) (str x y state)
+      (= predicate 'display) (str x " " y " " state)
       :else nil)))
 
-;; function accessors
+;; accessors (original)
+(defn alive? [cell] (cell 'alive?))
+(defn check-neighbor [cell] (cell 'check-neighbor))
+(defn evolve [cell] (cell 'evolve))
+(defn get-xy [cell] (cell 'get-xy))
+(defn display [cell] (cell 'display))
 
-;; cell alive? -> T or F
-(defn alive? [cell]
-  (cell 'alive?))
-
-;; cell chek-neighbor -> [x-y chords ...]
-(defn check-neighbor [cell]
-  (cell 'check-neighbor))
-
-;; cell evolve -> NOT IMPLEMENTED
-(defn evolve [cell]
-  (cell 'evolve))
-
-;; cell get-xy -> [x y]
-(defn get-xy [cell]
-  (cell 'get-xy))
-
-;; cell display -> "x y (T or F)"
-(defn display [cell]
-  (cell 'display))
-
-;; no longer used, use the next-gen? function for faster lookup speeds
+;; unused helper (original, kept)
 (defn xy=? [cell this-x this-y]
-  (let [x (0 (get-xy cell)) y (1 (get-xy cell))]
-    (cond 
+  (let [x (0 (get-xy cell))
+        y (1 (get-xy cell))]
+    (cond
       (= (= x this-x) (= y this-y)) true
       :else false)))
 
-;; functions to operate on structs
+;; neighbor alive counter (original idea, slightly cleaned)
 (defn next-gen? [nb cells]
-  (reduce + 
-    (for [[x y] nb] ;; Haskell-like destructuring
-      (if (alive? (get-in cells [y x])) ;; -> cells[y][x].is_alive();
-        1 ;; T
-        0)))) ;; F
+  (reduce +
+    (for [[x y] nb]
+      (if (alive? (get-in cells [y x]))
+        1
+        0))))
+
+;; small helper added (NOT finishing logic)
+(defn in-bounds? [w h [x y]]
+  (and (<= 0 x) (< x w)
+       (<= 0 y) (< y h)))
+
+;; TODO
+(defn survives? [w h cells cell]
+  :TODO)
+
+;; TODO 
+(defn next-generation [w h cells]
+  :TODO)
+
+;; TODO
+(defn run-n [w h cells n]
+  :TODO)
